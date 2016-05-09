07dd6dd33b1b9955a9944fc46fe57f9ee835ff3b
LolHens <pierrekisters@gmail.com>
2016-05-09 22:39:47 +0200

    not working, testing

--- scripts/docker-container/docker-container.sh
+++ scripts/docker-container/docker-container.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd /usr/local/docker-container/
-
 subcommand=$1
 shift
 
 if [[ -z `echo $subcommand | grep '^[^/]*/.*$'` ]]
 then
+  cd /usr/local/docker-container/
   cd .docker-container/
   sh "${subcommand}.sh" "$@"
 fi
