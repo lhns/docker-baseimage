ca5bcf553180e781d6fe03ea7bba442abe1e26d0
LolHens <pierrekisters@gmail.com>
2016-05-12 15:20:59 +0200

    testing appfolders link script

--- scripts/docker-containertools/.docker-containertools/.appfolders/link.sh
+++ scripts/docker-containertools/.docker-containertools/.appfolders/link.sh
@@ -7,9 +7,14 @@ then
     appname=`echo ${line} | grep -oP '^"\K([^"]*)(?=" )|^\K([^ ]*)(?= )'`
     name=`echo ${line} | grep -oP ' "\K([^"]*)(?=" )| \K([^ ]*)(?= )'`
     source=`echo ${line} | grep -oP ' "\K([^"]*)(?="$)| \K([^ ]*)(?=$)'`
+    target="/usr/local/appdata/${appname}/${name}/"
 
     echo "$appname"
     echo "$name"
     echo "$source"
+
+    mkdir "${target}"
+    cp -rf "${source}" "${target}"
+    ln "${target}" "${source}"
   done
 fi
